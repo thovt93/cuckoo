8bde47834d5d555e93ca3da2340355d43264bf5b
tho.vo <votantho93@gmail.com>
2019-04-28 22:44:27 +0700

    make image gravity center

--- app/serializers/admin/product_serializer.rb
+++ app/serializers/admin/product_serializer.rb
@@ -6,9 +6,11 @@ class Admin::ProductSerializer < Admin::ApplicationSerializer
     return unless product.images.attachments
     product.images.map do |image|
       ActiveStorage::Current.set(host: 'localhost:3000') do
-        image.variant(resize: '50x50!',
-                      gravity: 'center',
-                      background: 'white').processed.service_url
+        image.variant(combine_options: { gravity: 'center',
+                                         thumbnail: '50x50>',
+                                         extent: '50x50>',
+                                         background: 'white'
+                                       }).processed.service_url
       end
     end
   end
--- app/serializers/v1/product_serializer.rb
+++ app/serializers/v1/product_serializer.rb
@@ -6,9 +6,11 @@ class V1::ProductSerializer < V1::ApplicationSerializer
     return unless product.images.attachments
     product.images.map do |image|
       ActiveStorage::Current.set(host: 'localhost:3000') do
-        image.variant(resize: '50x50!',
-                      gravity: 'center',
-                      background: 'white').processed.service_url
+        image.variant(combine_options: { gravity: 'center',
+                                         thumbnail: '310x210>',
+                                         extent: '310x210>',
+                                         background: 'white'
+                                       }).processed.service_url
       end
     end
   end
